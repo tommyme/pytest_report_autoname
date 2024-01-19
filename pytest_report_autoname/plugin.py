387492f6c18dfe2b205a1a60002856524954fb7c
tommyme <chris.b.you@qq.com>
2024-01-19 09:34:01 +0800

    fix folder autocreate

--- pytest_report_autoname/plugin.py
+++ pytest_report_autoname/plugin.py
@@ -4,8 +4,9 @@ import os
 def pytest_load_initial_conftests(args):
     today=datetime.now()
     formatted_today=today.strftime('%y_%m_%d__%H_%M_%S')
-    if not os.path.exists("test_report"):
-        os.mkdir("test_report")
     for id, i in enumerate(args):
         if "--html" in i:
+            # 指定生成html的时候再创建文件夹
+            if not os.path.exists("test_report"):
+                os.mkdir("test_report")
             args[id] = "--html={}/report-{}.html".format('test_report',formatted_today)
